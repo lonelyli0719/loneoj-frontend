c44c652f717e1a5a0e2cce87bba432977e9498fb
lonelyli0719 <794156416@qq.com>
2025-08-02 20:03:28 +0800

    loneOj前端初始化以及基础菜单开发

---
+++ src/router/routes.ts
@@ -0,0 +1,41 @@
+import { RouteRecordRaw } from "vue-router";
+import HomeView from "@/views/HomeView.vue";
+import AccessEnum from "@/access/accessEnum";
+
+export const routes: Array<RouteRecordRaw> = [
+  {
+    path: "/",
+    name: "home",
+    component: HomeView,
+  },
+  {
+    path: "/hide",
+    name: "隐藏页面",
+    component: HomeView,
+    meta: {
+      hideInMenu: true,
+    },
+  },
+  {
+    path: "/noAuth",
+    name: "无权限",
+    component: () => import("../views/NoAuthView.vue"),
+  },
+  {
+    path: "/admin",
+    name: "admin",
+    component: () => import("../views/AdminView.vue"),
+    meta: {
+      access: AccessEnum.ADMIN,
+    },
+  },
+  {
+    path: "/about",
+    name: "about",
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views/AboutView.vue"),
+  },
+];
