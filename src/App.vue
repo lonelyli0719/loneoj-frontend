e85e52e6ec99ab048136d8906e70bafa55d0d549
lonelyli0719 <794156416@qq.com>
2025-08-02 22:59:38 +0800

    loneOj前后端对接

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,29 @@
 <template>
   <div id="app">
-    <BasicLayout></BasicLayout>
+    <template v-if="route.path.startsWith('/user')">
+      <router-view></router-view>
+    </template>
+    <template v-else>
+      <BasicLayout></BasicLayout>
+    </template>
   </div>
 </template>
 
 <style></style>
 <script setup lang="ts">
 import BasicLayout from "@/layouts/BasicLayout.vue";
-import { useRouter } from "vue-router";
-import { useStore } from "vuex";
 import { onMounted } from "vue";
+import { useRoute } from "vue-router";
+
+const route = useRoute();
 
 /**
- * 全局初始化函数，有全局单次调用的代码，都可以写到这里
+ * 全局初始化函数，有全局单次调用的代码。都可以写到这里
  */
 const doInit = () => {
   console.log("hello 欢迎来到我的项目");
 };
-
 onMounted(() => {
   doInit();
-});
-
-const router = useRouter();
-const store = useStore();
-router.beforeEach((to, from, next) => {
-  if (to.meta.access === "canAdmin") {
-    if (store.state.loginUser?.role !== "admin") {
-      next("/noAuth");
-      return;
-    }
-  }
-  next();
 });
 </script>
--- src/router/routes.ts
+++ src/router/routes.ts
@@ -1,8 +1,28 @@
 import { RouteRecordRaw } from "vue-router";
 import HomeView from "@/views/HomeView.vue";
 import AccessEnum from "@/access/accessEnum";
+import UserLoginView from "@/views/user/UserLoginView.vue";
+import UserLayout from "@/layouts/UserLayout.vue";
+import UserRegisterView from "@/views/user/UserRegisterView.vue";
 
 export const routes: Array<RouteRecordRaw> = [
+  {
+    path: "/user",
+    name: "用户",
+    component: UserLayout,
+    children: [
+      {
+        path: "/user/login",
+        name: "用户登录",
+        component: UserLoginView,
+      },
+      {
+        path: "/user/register",
+        name: "用户注册",
+        component: UserRegisterView,
+      },
+    ],
+  },
   {
     path: "/",
     name: "home",
